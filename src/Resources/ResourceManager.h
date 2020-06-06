47385e417dc7fb42c51fd9a99b251f51950aa78d
Maxim <shalaginmx@mail.ru>
2020-06-06 11:10:36 +0300

    small changes in code style

--- src/Resources/ResourceManager.h
+++ src/Resources/ResourceManager.h
@@ -28,15 +28,13 @@ public:
 
     ResourceManager(const ResourceManager &&) = delete;
 
-    std::shared_ptr<Renderer::ShaderProgram>
-    loadShaderProgram(const std::string &shaderName, const std::string &vertexPath, const std::string &fragmentPath);
-
-    std::shared_ptr<Renderer::ShaderProgram>
-    getShader(const std::string &shaderName);
+    std::shared_ptr<Renderer::ShaderProgram> loadShaderProgram(const std::string &shaderName,
+                                                                const std::string &vertexPath,
+                                                                const std::string &fragmentPath);
+    std::shared_ptr<Renderer::ShaderProgram> getShader(const std::string &shaderName);
 
     std::shared_ptr<Renderer::Texture2D> loadTexture(const std::string & textureName, const std::string& texturePath);
-    std::shared_ptr<Renderer::Texture2D>
-    getTexture(const std::string &textureName);
+    std::shared_ptr<Renderer::Texture2D> getTexture(const std::string &textureName);
 
 private:
     std::string getFileString(const std::string& relativeFilePath) const;
